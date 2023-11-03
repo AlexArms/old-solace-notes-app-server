5d994adb58b2e9b781ab444309d3cb36a1e561a3
Alex <alexaarms96@gmail.com>
2023-11-02 22:45:51 -0600

    added cors middleware

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 const jsonParser = bodyParser.json();
 
 app.use(jsonParser);
+app.use(cors());
 
 const users = require("./routes/users");
 const notes = require("./routes/notes");
